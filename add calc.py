c6ad5a1616091c3e660f655bb454e97d375dbc8c
Hanna2604 <hannaammu0405@gmail.com>
2025-05-13 23:10:27 +0530

    Add files via upload

---
+++ add calc.py
@@ -0,0 +1,5 @@
+print("calculator")
+num1=int(input("enter the first number"))
+num2=int (input("enter second number"))
+sum=num1+num2
+print(f"sum is {num1}+{num2}={sum}")
---
+++ control flow.py
@@ -0,0 +1,7 @@
+print("multiplication tble")
+num=int(input("enter the number="))
+for i in range(1,11):
+    print(f"{num}x{i}={i*num}")
+    
+
+
